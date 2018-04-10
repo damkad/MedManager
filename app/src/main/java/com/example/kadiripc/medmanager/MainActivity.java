2cbc09f353bc8486718d51f36cd4b920873f800c
damkad <dammy012>
2018-04-10 08:01:01 +0100

    new development

--- app/src/main/java/com/example/kadiripc/medmanager/MainActivity.java
+++ app/src/main/java/com/example/kadiripc/medmanager/MainActivity.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.kadiripc.medmanager.adapter.medicineManagerAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recycler)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        medicineManagerAdapter medAdapter = new medicineManagerAdapter(this);
+        recyclerView.setAdapter(medAdapter);
+
+        LinearLayoutManager lM = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(lM);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
